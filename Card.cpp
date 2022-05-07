8f66a09f3d6d0c18cd425f4332bbd3f14c7ee86a
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2022-05-07 22:09:49 +0900

    Add files via upload

---
+++ Card.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <deque>
+
+int main() {
+
+	int N, k;
+	int* num;
+
+	std::deque<int> d;
+
+	std::cin >> N;
+
+	num = (int*)malloc(sizeof(int) * N);
+	
+	for (int i = 0; i < N; i++) {
+		std::cin >> num[i];
+	}
+	
+	for (int i = N - 1; i >= 0; i--) {
+		if (num[i] == 1) {
+			d.push_front(N - i);
+		}
+		else if (num[i] == 2) {
+			k = d.front();
+			d.pop_front();
+			d.push_front(N - i);
+			d.push_front(k);
+		}
+		else {
+			d.push_back(N - i);
+		}
+	}
+	
+	for (int i = 0; i < N; i++) {
+		std::cout << d[i] << " ";
+	}
+
+	free(num);
+}
---
+++ RotationQueue.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <deque>
+
+int main() {
+
+	int N, M, count = 0;
+
+	std::cin >> N;
+	std::cin >> M;
+
+	std::deque<int> d;
+
+	for (int i = 1;  i <= N; i++) {
+		d.push_back(i);
+	}
+	
+	int num[50];
+	for (int i = 0; i < M; i++) {
+		std::cin >> num[i];
+	}
+	
+	for (int i = 0; i < M; i++) {
+		int fnum = 0, bnum = 0;
+		for (int j = 0; j < d.size(); j++) {
+			if (num[i] == d[j]) break;
+			fnum++;
+		}
+		for (int j = d.size() - 1; j >= 0; j--) {
+			if (num[i] == d[j]) break;
+			bnum++;
+		}
+		bnum++;
+
+		if (num[i] == d.front()) {
+			d.pop_front();
+		}
+		else if (fnum < bnum) {
+			for (int j = 0; j < fnum; j++) {
+				d.push_back(d.front());
+				d.pop_front();
+				count++;
+			}
+			d.pop_front();
+		}
+		else {
+			for (int j = 0; j < bnum; j++) {
+				d.push_front(d.back());
+				d.pop_back();
+				count++;
+			}
+			d.pop_front();
+		}		
+	}
+	std::cout << count;
+}
